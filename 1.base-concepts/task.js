9db12e7016d796b5079851520f5d0ea1541e0982
SlavaFors <furtive7@yandex.ru>
2022-06-14 16:02:07 +0300

    Задание 1 Задача 1 выполнены

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,46 @@
+'use strict';
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr = [];
+  let discriminant = b**2 - 4 * a * c;
+  let discriminantRoot1;
+  let discriminantRoot2;
+
+  if (discriminant < 0) {
+  	let arr = [];
+  } else if (discriminant > 0) {
+  	discriminantRoot1 = (-b + Math.sqrt(discriminant) ) / (2 * a);
+    discriminantRoot2 = (-b - Math.sqrt(discriminant) ) / (2 * a);
+    arr.push(discriminantRoot1, discriminantRoot2);
+  } else if (discriminant === 0) {
+  	discriminantRoot1 = -b / (2 * a);
+  	arr.push (discriminantRoot1);
+  } 
+
   return arr; // array
 }
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
+
+//-----------------------------------------------------------------------//
+//function calculateTotalMortgage(percent, contribution, amount, date) { 
+//процентная ставка, сумма первоначального взноса, сумма кредита и срок (дата окончания кредита)
+ // let totalAmount;
+ // let creditBody = amount - contribution;
+
 
   // код для задачи №2 писать здесь
+	//+ Посчитайте тело кредита: сумма, которую необходимо вернуть банку (сумма кредита минус первоначальный взнос).
+	//Посчитайте на какой срок был выдан кредит (в месяцах).
+	//Ежемесячная оплата рассчитывается по формуле: Платеж = S * (P + (P / (((1 + P)^n) - 1))), где: 
+	//+S - тело кредита, P - 1/12 процентной ставки (от 0 до 1), n - количество месяцев ^ - возведение в степень
+	//Посчитайте общую сумму, которую придется заплатить клиенту.
+	//Округлите результат до двух значений после запятой.
+	//Выведите результат в консоль, а также верните его из функции. Результатом функции должно 
+	//быть значение числового типа.
+  	//Обратите внимание, что в инпуте пользователь указывает конечную дату, 
+  	//поэтому в функции необходимо посчитать срок в месяцах на основе введенной даты. 
+  	//(вам потребуется использовать встроенный объект Date)
 
-  return totalAmount;
-}
+
+ // return totalAmount;
+//}
