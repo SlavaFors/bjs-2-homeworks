f099374f6ad9924bb560561a1d56ff7dc5f83662
SlavaFors <furtive7@yandex.ru>
2022-07-21 17:02:22 +0300

    V3

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -2,85 +2,55 @@ function cachingDecoratorNew(func) {
 	let cache = [];
 
 	function wrapper(...args) {
-	    const hash = args.join(', '); 
-	    let objectInCache = cache.findIndex(item => item.hash === hash); 	    
+		const hash = args.join(', '); 
+		let objectInCache = cache.findIndex(item => item.hash === hash);
 	    
-	    if (objectInCache !== -1) {
-	        console.log("Из кэша: " + cache[objectInCache].result); 
-	        return "Из кэша: " + cache[objectInCache].result;
-	    } else {
+		if (objectInCache !== -1) {
+				console.log("Из кэша: " + cache[objectInCache].result); 
+				return "Из кэша: " + cache[objectInCache].result;
+		} 
 
-	    let result = func(...args); 
-	    cache.push({hash, result}); 
-	    if (cache.length > 5) { 
-	      cache.shift(); 
-	    }
-	    console.log("Вычисляем: " + result);
-	    return "Вычисляем: " + result;  
-	}
- };	
+		let result = func(...args); 
+		cache.push({hash, result}); 
+		if (cache.length > 5) { 
+			cache.shift(); 
+		}
+		console.log("Вычисляем: " + result);
+		return "Вычисляем: " + result;  
+		}
 	return wrapper;
 }
 
 
 
 function debounceDecoratorNew(func, delay) {
-	let isThrottled = true;
-	let timeout;
+	let timeout = null;
 
-	return function (...args) {
-		if (isThrottled) {
-		func.apply(this, args);
-	//return isThrottled = false;
-	} //else if (isThrottled === false) {
-		//clearTimeout(timeout);
-	timeout = setTimeout(() => {
-		func.apply(this, args);
-	}, delay);
+	function wrapper (...args) {
+		if (timeout === null) {
+			func(...args);
+	  }
+	  clearTimeout(timeout);
+	  timeout = setTimeout(() => func(...args), delay);
+	}
+	return wrapper;
 }
-}
+
+
 
 
 function debounceDecorator2(func,delay) {
-	let isThrottled = true;
-	let timeout;
+	let timeout = null;
 	
 	function wrapper (...args){
-		wrapper.count = 0;
-		
-		if (isThrottled) {
-			func.apply(this, args);
-			wrapper.count.push (wrapper.count++);
-			
-			return func (wrapper.count);
-			//return isThrottled = false;
-		} //else if (isThrottled === false) {
-			//clearTimeout(timeout);
-		timeout = setTimeout(() => {
-			func.apply(this, args);
-		}, delay);
-} return wrapper;
+		if (timeout === null) {
+			func(...args);
+			clearTimeout(timeout);
+			timeout = setTimeout(() => func(...args), delay)
+		} 
+		wrapper.count++;
+	} 
+	wrapper.count = 0;
+	return wrapper;
 }
-
-/*function debounceDecorator2(func,delay) {
-	let isThrottled = true;
-	let timeout;
-	let count = 0; 
-
-return function wrapper (...args){
-	count++;
-	wrapper.amount = count;
-	console.log(wrapper.amount); 
-
-	if (isThrottled) {
-		func.apply(this, args);
-		//return isThrottled = false;
-	} //else if (isThrottled === false) {
-		//clearTimeout(timeout);
-
-	timeout = setTimeout(() => {
-		func.apply(this, args);
-	}, delay);
-}
-}*/
 
